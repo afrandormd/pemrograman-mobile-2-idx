6c94cd348bf04d5139761d209859685f7d8af798
afrandormd <afrandormd@gmail.com>
2024-11-19 11:02:43 +0000

    menambahkan fitur add dari modul

--- lib/app/modules/mahasiswa/controllers/mahasiswa_controller.dart
+++ lib/app/modules/mahasiswa/controllers/mahasiswa_controller.dart
@@ -1,7 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class MahasiswaController extends GetxController {
+// Implement mahasiswa controller
+  late TextEditingController cNpm;
+  late TextEditingController cNama;
+  late TextEditingController cAlamat;
+
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   Future<QuerySnapshot<Object?>> GetData() async {
@@ -12,5 +18,55 @@ class MahasiswaController extends GetxController {
   Stream<QuerySnapshot<Object?>> StreamData() {
     CollectionReference mahasiswa = firestore.collection('mahasiswa');
     return mahasiswa.snapshots();
+  }
+
+  void add(
+    String npm,
+    String nama,
+    String alamat,
+  ) async {
+    CollectionReference mahasiswa = firestore.collection("mahasiswa");
+
+    try {
+      await mahasiswa.add({
+        "npm": npm,
+        "nama": nama,
+        "alamat": alamat,
+      });
+      Get.defaultDialog(
+          title: "Berhasil",
+          middleText: "Berhasil menyimpan data mahasiswa",
+          onConfirm: () {
+            cNpm.clear();
+            cNama.clear();
+            cAlamat.clear();
+            Get.back();
+            Get.back();
+            textConfirm:
+            "OK";
+          });
+    } catch (e) {
+      print(e);
+      Get.defaultDialog(
+        title: "Terjadi Kesalahan",
+        middleText: "Gagal Menambahkan Mahasiswa.",
+      );
+    }
+  }
+
+  @override
+  void onInit() {
+    cNpm = TextEditingController();
+    cNama = TextEditingController();
+    cAlamat = TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    //implementasi onClose
+    cNpm.dispose();
+    cNama.dispose();
+    cAlamat.dispose();
   }
 }
